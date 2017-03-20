71137d9ab420793d56b6b0aae074816be5026b06
Nicole Cordes <typo3@cordes.co>
2017-03-20 20:12:50 +0100

    [BUGFIX] Ensure correct mysqli resource for admin_get_tables query
    The admin_get_tables method tries to fetch all available database tables
    from the default connection. But using the native mode, the query is
    executed with the $this->lastHandlerKey link which might not be the
    default connection. This patch ensures the correct mysqli resource is
    used to resolve the database queries.
    
    See: https://forge.typo3.org/issues/80358
    
    Resolves: #1

--- Classes/Database/DatabaseConnection.php
+++ Classes/Database/DatabaseConnection.php
@@ -2695,6 +2695,7 @@ class DatabaseConnection extends \TYPO3\CMS\Core\Database\DatabaseConnection
     {
         $whichTables = [];
         // Getting real list of tables:
+        $this->lastHandlerKey = '_DEFAULT';
         switch ($this->handlerCfg['_DEFAULT']['type']) {
             case 'native':
                 $tables_result = $this->query('SHOW TABLE STATUS FROM `' . $this->handlerCfg['_DEFAULT']['config']['database'] . '`');
@@ -2702,8 +2703,8 @@ class DatabaseConnection extends \TYPO3\CMS\Core\Database\DatabaseConnection
                     while ($theTable = $this->sql_fetch_assoc($tables_result)) {
                         $whichTables[$theTable['Name']] = $theTable;
                     }
+                    $tables_result->free();
                 }
-                $tables_result->free();
                 break;
             case 'adodb':
                 // check needed for install tool - otherwise it will just die because the call to
